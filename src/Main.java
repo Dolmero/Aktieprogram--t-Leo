f184613cc0573b0110044fc7652f0a4e83bf2551
Gustav K <g-u-r-r-a@hotmail.com>
2018-01-28 21:04:41 +0100

    q

--- src/Main.java
+++ src/Main.java
@@ -8,7 +8,7 @@ import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 
 import javax.swing.*;
-
+okay
 public class Main {
 	static float preValue, curValue, result, x;
 	static JTextArea resultbox = new JTextArea(12,35);
