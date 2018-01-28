f7fb639075dafde15ccf4b58f8a4b0b68463dcb3
Gustav K <g-u-r-r-a@hotmail.com>
2018-01-28 20:52:57 +0100

    q

--- src/Main.java
+++ src/Main.java
@@ -8,8 +8,8 @@ import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 
 import javax.swing.*;
-q
-public class Main {
+
+public class Main {s
 	static float preValue, curValue, result, x;
 	static JTextArea resultbox = new JTextArea(12,35);
 	static JTextField name = new JTextField(24);
